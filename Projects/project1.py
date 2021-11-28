c4a1c003504d767125be285b2db261363c81d57b
MonHardy <munchenhardy@gmail.com>
2021-11-28 16:20:04 -0300

    Create project1.py

---
+++ Projects/project1.py
@@ -0,0 +1,13 @@
+print("Welcome to the Band Name Generator.\n")
+
+city = input("What's name of the city you grew up in?\n")
+str1 = city
+
+#
+
+pet = input("What's your pet's name?\n")
+str2 = pet
+
+#
+print("your band name could be " + str1 + " " + str2)
+
