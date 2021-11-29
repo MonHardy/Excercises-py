62292bb9fabe87c4bf58e015763c0b2f09f95596
Mon <munchenhardy@gmail.com>
2021-11-28 23:34:52 -0300

    Update project-d2-1.py

--- Projects/project-d2-1.py
+++ Projects/project-d2-1.py
@@ -1,9 +1,6 @@
-# 🚨 Don't change the code below 👇
+#Write a program that adds the digits in a 2 digit number. 
 two_digit_number = input("Type a two digit number: ")
-# 🚨 Don't change the code above 👆
 
-####################################
-#Write your code below this line 👇
 
 number = int(two_digit_number[0])
 number2 = int(two_digit_number[1])
