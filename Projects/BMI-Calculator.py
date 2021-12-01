295d7549bda52577d51c326c50750305a2275c46
Mon <munchenhardy@gmail.com>
2021-12-01 19:36:36 -0300

    att

--- Projects/BMI-Calculator.py
+++ Projects/BMI-Calculator.py
@@ -1,6 +1,8 @@
-print('Welcome! This is a BMI calculator, please let me know...')
+print('Welcome! \n This is a BMI calculator, please let me know...\n')
+print("\n ** Exemple: \n height: 1.8 \n weight: 60 **\n")
 height = float(input("Your height in m: "))
 weight = int(input("Your weight in kg: "))
+
 
 bmi = int(weight) / float(height) ** 2
 bmi_as_int = int(bmi)
